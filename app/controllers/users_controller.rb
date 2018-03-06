f46d29a0f40893e4c759c2593e3428bfb9f5a5a3
strivert <strivert@email.com>
2018-03-06 10:12:32 +0800

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params) #{"name"=>"myName", "email"=>"my@email.com", "password"=>"123123", "password_confirmation"=>"123123"}    
+    if @user.save
+      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # redirect_to user_url(@user) = Get /users/3 = Get user_path(user) = show action, also think about user_path(@user)
+      # user_path(@user) = "/users/1"
+      # user_url(@user) = "http://localhost:3000/users/1"
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,7 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
     get root_path
-    assert_template 'static_pages/home'
+    assert_template 'static_pages/home' # controller/action
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", helf_path
     assert_select "a[href=?]", about_path
