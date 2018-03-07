01573f96837c95fa9ed53d969e723d0ed93d8b90
strivert <strivert@email.com>
2018-03-07 09:26:10 +0800

    advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user # log_in is from ../helpers/sessions_helper.rb
+      #remember user # remember is from ../helpers/sessions_helper.rb
+      # if remember is checked = rmember(user) or if unchecked, forget(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to current_user # user_url(user) => show action
     else
       # Create an error message.
@@ -16,7 +19,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out # log_out is from ../helpers/sessions_helper.rb
+    log_out if logged_in? # log_out is from ../helpers/sessions_helper.rb, "if logged_in" => for first subtle
     redirect_to root_url
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -68,6 +68,11 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  # for an subtle
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
 
 # others - 
